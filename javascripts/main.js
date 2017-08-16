e7489a7fae6efd3f5006f726bb585a5e96d7b6eb
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-08-16 16:22:17 -0500

    updates

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,9 @@
 "use strict";
 console.log("Main.js, Yo!");
 
-let colorSquares = require("./color-squares");
-let eventListeners = require("./event-listeners");
-let areInfo = require("./area-info");
-
+let colorSquares = require("./color-squares.js");
+let eventListeners = require("./event-listeners.js");
+let areInfo = require("./area-info.js");
+let timepicker = require("./time_picker.js");
 
 
