1b7b58ae550d25534e45fbf4753b9ee0b9afe409
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-08-16 16:20:25 -0500

    Added required and cdn for timepicker

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,6 +4,6 @@ console.log("Main.js, Yo!");
 let colorSquares = require("./color-squares");
 let eventListeners = require("./event-listeners");
 let areInfo = require("./area-info");
-
-
+let time_picker = require("./time_picker");
+let search = require("./search");
 
