c3267875043dfb0f4b419513409ce3e0c85a684f
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-08-17 16:36:50 -0500

    updated output

--- javascripts/color-squares.js
+++ javascripts/color-squares.js
@@ -37,9 +37,11 @@ function showAreas(data) {
                 function convertHexG(hex) {return parseInt((removeHash(hex)).substring(2,4),16);}
                 function convertHexB(hex) {return parseInt((removeHash(hex)).substring(4,6),16);}
                 function removeHash(hex) {return (hex.charAt(0)=="#") ? hex.substring(1,7):hex;}
-                    // console.log(R,G,B);
                     let alhpaColor = `rgba(${R},${G},${B},.4)`;
                 $(`#box--${indAreaID}`).css("background-color", `${alhpaColor}`);
+                $("#output").html(`<h5>The very best of days to you!</h5> <p><i>Now, to our new students, welcome, to our old students, welcome back! Another year full of magical education awaits you!</i> <br>—Albus Dumbledore</p>
+                <p>Check out the areas in <b>Hogwarts</b> to see what wonderful attractions await you.</p>
+                <p><i><b>You'll need to use your wand!</i></b><p>`);
                 console.log(alhpaColor);
             });
         }
@@ -50,6 +52,6 @@ function hideAreas(data) {
             $.each(keys,(index, item) => {
                 indAreaID = data[item].id;
                 $(`#box--${indAreaID}`).css("background", `none`);
-                $("#output").html("Come Back Soon!");
+                $("#output").html(`<p></p><h2>Come Back Soon!</h2>`);
             });
         }
